0a80df99388d0b0c78c8984ec06ed3ee86259804
e.covello <emanuele.covello@gmail.com>
2026-05-22 11:07:26 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <main className="flex flex-1 w-full">
+        <h1>Resume Builder</h1>
+      </main>
+    </div>
+  );
+}
